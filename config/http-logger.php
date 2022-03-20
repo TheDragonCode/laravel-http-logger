f67258ba2abb62d7406fce0843f3aa05b4758975
Andrey Helldar <helldar@ai-rus.com>
2022-03-21 00:27:44 +0300

    Added the ability to disable the hiding of secret keys

--- config/http-logger.php
+++ config/http-logger.php
@@ -1,17 +1,21 @@
 <?php
 
 return [
-    'enabled' => env('LOG_HTTP_ENABLED', true),
+    'enabled' => env('HTTP_LOG_ENABLED', true),
 
     'connection' => env('DB_CONNECTION'),
 
     'table' => 'http_logs',
 
     'hide' => [
-        'authorization',
-        'token',
-        'access_token',
-        'password',
-        'password_confirmation',
+        'enabled' => env('HTTP_LOG_HIDE_ENABLED', true),
+
+        'keys' => [
+            'authorization',
+            'token',
+            'access_token',
+            'password',
+            'password_confirmation',
+        ],
     ],
 ];
